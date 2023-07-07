838964ab546ac6c262f5edd37c0d33286d066a9a
norheim <norheim@mit.edu>
2023-07-07 11:54:03 +0200

    optimizer running

--- minimdo/modeling/arghandling.py
+++ minimdo/modeling/arghandling.py
@@ -1,7 +1,10 @@
 import numpy as np
+from itertools import chain
 
 def flatten_args(args):
-    return np.concatenate([np.array(arg).flatten() for arg in args])
+    return (np.concatenate([np.array(arg).flatten() 
+                           for arg in args]) if len(args) 
+                           > 0 else np.array([]))
 
 def scalar_if_possible(array_or_float):
     squeezed = np.squeeze(array_or_float)
@@ -22,8 +25,10 @@ def unflatten_args(x, shapes=None, cleanup=False):
         x = x[size:]
     return unflattened
 
-def encode(d, order, flatten=False):
-    encoded = tuple(d[var] for var in order)
+def encode(d, order, flatten=False, missingarg=None):
+    encoded = tuple(d.get(var, 
+                          missingarg() if missingarg is not None else None) 
+                          for var in order)
     if flatten:
         encoded = flatten_args(encoded)
     return encoded
@@ -34,3 +39,65 @@ def decode(x, order, shapes=None, unflatten=False, cleanup=False):
     if unflatten:
         x = unflatten_args(x, shapes, cleanup)
     return {var: x[idx] for idx, var in enumerate(order)}
+
+class Encoder():
+    def __init__(self, order=None, shapes=None, parent=None):
+        self.order = order if order is not None else tuple()
+        self.parent = parent
+        self.shapes = shapes if shapes is not None else ((1,),)*len(self.order)
+
+def merge_encoders(*encoders, exclude_encoder=None):
+    order = tuple()
+    shapes = tuple()
+    exclude_order = exclude_encoder.order if exclude_encoder is not None else tuple()
+    for encoder in encoders:
+        for hashable,shape in zip(encoder.order, encoder.shapes):
+            if hashable not in order+exclude_order:
+                order += (hashable,)
+                shapes += (shape,)
+    return Encoder(order, shapes, parent=None)
+
+class EncodedFunction():
+    def __init__(self, f, encoder=None, decoder=None, **kwargs):
+        self.f = f
+        self.kwargs = kwargs
+        self.encoder = encoder if encoder is not None else Encoder()
+        self.decoder = decoder if decoder is not None else Encoder()
+
+    def dict_out_only(self, *args, **kwargs):
+        return decode(self.f(*args, **{**self.kwargs, **kwargs}), 
+                      self.decoder.order, shapes=self.decoder.shapes)
+
+    def dict_in_only(self, d, **kwargs):
+        return self.f(*encode(d, self.encoder.order), **{**self.kwargs, **kwargs})
+    
+    def dict_in_flat_out(self, d, **kwargs):
+        return flatten_args(self.dict_in_only(d, **{**self.kwargs, **kwargs}))
+    
+    def dict_in_dict_out(self, d, **kwargs):
+        return decode(self.dict_in_only(d, **{**self.kwargs, **kwargs}), 
+                      self.decoder.order, shapes=self.decoder.shapes)
+    
+    def __repr__(self) -> str:
+        return '{} <- {}'.format(str(self.decoder.order), 
+                                  str(self.encoder.order))
+    
+class EncodedFunctionContainer(EncodedFunction):
+    def __init__(self, encoded_functions=None, f=None, **kwargs):
+        super().__init__(f, **kwargs)
+        self.encoded_functions = list()
+        encoded_functions = encoded_functions if encoded_functions is not None else tuple()
+        self.add_encoded_functions(*encoded_functions)
+
+    def add_encoded_functions(self, *encoded_functions):
+        self.encoded_functions.extend(encoded_functions)
+        self.decoder = merge_encoders(*chain((self.decoder,), 
+            (encoded_function.decoder for encoded_function 
+             in encoded_functions)))
+        self.encoder = merge_encoders(*chain((self.encoder,),
+            (encoded_function.encoder for encoded_function 
+             in encoded_functions)), exclude_encoder=self.decoder)
+    
+    def __repr__(self) -> str:
+        joinlist = [str(elt) for elt in self.encoded_functions]
+        return '\n'.join(joinlist)
